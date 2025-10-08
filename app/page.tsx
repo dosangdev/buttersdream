00abe2f054c39e2f52644b13687a9b501d591cff
dosangdev <sangdo1113@naver.com>
2025-10-08 16:38:56 +0900

    타입오류 수정

--- app/page.tsx
+++ app/page.tsx
@@ -217,9 +217,9 @@ export default function Home() {
                       }}
                       onClick={() => {
                         // Farcaster 프로필 링크는 donors.ts에 정보가 없으므로 주석처리
-                        window.open(
-                          `https://farcaster.xyz/${item.farcasterUserData?.username}`
-                        );
+                        // window.open(
+                        //   `https://farcaster.xyz/${item.farcasterUserData?.username}`
+                        // );
                       }}
                     >
                       <motion.div
